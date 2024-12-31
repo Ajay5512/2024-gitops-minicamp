6134c589b0e14cf9cc8fd2ae8519aef9951d35c7
Alexio Junior Aaron <97569992+Ajay263@users.noreply.github.com>
2024-12-31 07:25:10 +0000

    Added s3 bucket and dynamo

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,9 +1,25 @@
 terraform {
-  required_version = ">= 0.14"
+  required_version = "~> 1.9.5"
+  
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.0"
+      version = "5.69.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "3.4.5"
     }
   }
+
+  backend "s3" {
+    bucket         = "gitops-tf-backend"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "GitopsTerraformLocks"
+  }
+}
+
+provider "aws" {
+  region = var.region
 }
