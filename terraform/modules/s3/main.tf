1d258b178638307b592c515f958e5e95cfd07b78
ALEXIO JUNIOR AARON <76625390+Ajay5512@users.noreply.github.com>
2025-01-03 22:57:53 +0000

    Updated s3 buckets

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -1,6 +1,11 @@
 # modules/s3/main.tf
+
 resource "aws_s3_bucket" "source_bucket" {
   bucket = "topdevs-${var.environment}-${var.source_bucket}"
+}
+
+resource "aws_s3_object" "organizations_file" {
+  bucket = aws_s3_bucket.source_bucket.id
   key    = "organizations.csv"
   source = "../data/organizations.csv"
 }
@@ -13,13 +18,13 @@ resource "aws_s3_bucket" "code_bucket" {
   bucket = "topdevs-${var.environment}-${var.code_bucket}"
 }
 
-resource "aws_s3_bucket_object" "glue_script" {
+resource "aws_s3_object" "glue_script" {
   bucket = aws_s3_bucket.code_bucket.id
   key    = "script.py"
   source = var.script_path
 }
 
-resource "aws_s3_bucket_object" "schema_change_script" {
+resource "aws_s3_object" "schema_change_script" {
   bucket = aws_s3_bucket.code_bucket.id
   key    = "schema_change.py"
   source = var.schema_change_script_path
