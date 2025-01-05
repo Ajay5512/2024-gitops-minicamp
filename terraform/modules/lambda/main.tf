3217d9ece17d5edca90274c594ed12c3e8ba4571
ALEXIO JUNIOR AARON <76625390+Ajay5512@users.noreply.github.com>
2025-01-05 18:33:22 +0000

    Added lambda triggers

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -83,6 +83,8 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
     lambda_function_arn = aws_lambda_function.s3_copy_lambda.arn
     events              = ["s3:ObjectCreated:*"]
   }
+
+  depends_on = [aws_lambda_permission.allow_s3]
 }
 
 resource "aws_lambda_permission" "allow_s3" {
