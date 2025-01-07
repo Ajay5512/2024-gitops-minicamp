c78a3657008e24d698f559c700a8920e060f8da7
ALEXIO JUNIOR AARON <76625390+Ajay5512@users.noreply.github.com>
2025-01-07 08:42:48 +0000

    Updated vpc variables

--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -1,10 +1,4 @@
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
 
-output "private_subnet_ids" {
-  value = aws_subnet.private[*].id
-}
 
 output "vpc_endpoint_s3_id" {
   value = aws_vpc_endpoint.s3.id
