435a82d165132135f3a5eaafd9dd1deb088ac590
Ajay263 <junioralexio607@gmail.com>
2025-01-10 15:07:44 +0200

    Updated sns

--- terraform/modules/sns/main.tf
+++ terraform/modules/sns/main.tf
@@ -3,6 +3,8 @@ resource "aws_sns_topic" "schema_changes" {
   name = "topdevs-${var.environment}-schema-changes"
 }
 
+
+
 resource "aws_sns_topic_policy" "schema_changes" {
   arn = aws_sns_topic.schema_changes.arn
   policy = jsonencode({
--- terraform/modules/sns/outputs.tf
+++ terraform/modules/sns/outputs.tf
@@ -3,3 +3,5 @@
 output "topic_arn" {
   value = aws_sns_topic.schema_changes.arn
 }
+
+
