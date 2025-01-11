2006076e5609af1711717a72e0ce03ea6e964784
Ajay263 <junioralexio607@gmail.com>
2025-01-11 04:15:09 +0200

    Updated the glue python job configs

--- terraform/modules/sns/main.tf
+++ terraform/modules/sns/main.tf
@@ -4,24 +4,3 @@ resource "aws_sns_topic" "schema_changes" {
 }
 
 
-
-# resource "aws_sns_topic_policy" "schema_changes" {
-#   arn = aws_sns_topic.schema_changes.arn
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Sid    = "AllowGluePublish"
-#         Effect = "Allow"
-#         Principal = {
-#           AWS = var.glue_role_arn
-#         }
-#         Action   = "SNS:Publish"
-#         Resource = aws_sns_topic.schema_changes.arn
-#       }
-#     ]
-#   })
-# }
-
-
-
