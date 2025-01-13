b97a42df2e43769f74167aaba445ac7c0785bda4
Ajay263 <junioralexio607@gmail.com>
2025-01-13 18:58:42 +0200

    Removed keypair variable

--- terraform/modules/ec2/variables.tf
+++ terraform/modules/ec2/variables.tf
@@ -13,10 +13,6 @@ variable "instance_type" {
   description = "Type of EC2 instance"
 }
 
-variable "key_name" {
-  type        = string
-  description = "Name of the key pair to use for SSH access"
-}
 
 variable "vpc_id" {
   type        = string
