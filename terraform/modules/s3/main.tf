770b637e66cea3d2ebe92b3e2b09968039dc74f3
Ajay263 <junioralexio607@gmail.com>
2025-01-21 13:21:30 +0200

    Updated

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -1,3 +1,5 @@
+# main.tf
+
 # Source Bucket
 resource "aws_s3_bucket" "source_bucket" {
   bucket = "nexabrands-${var.environment}-${var.source_bucket}"
@@ -5,15 +7,12 @@ resource "aws_s3_bucket" "source_bucket" {
   force_destroy = true
 }
 
-# Upload objects to source bucket with automatic folder organization# main.tf
+# Upload objects to source bucket
 resource "aws_s3_object" "source_files" {
-  for_each = {
-    for filename, filepath in var.source_files :
-    "${trimsuffix(filename, ".csv")}/${filename}" => filepath
-  }
+  for_each = var.source_files
 
   bucket                 = aws_s3_bucket.source_bucket.id
-  key                    = each.key
+  key                    = "data/${each.key}"
   source                 = each.value
   server_side_encryption = "aws:kms"
   kms_key_id            = aws_kms_key.s3_kms_key.arn
@@ -192,4 +191,79 @@ resource "aws_s3_bucket_object_lock_configuration" "target_bucket_lock" {
   }
 
   depends_on = [aws_s3_bucket_versioning.target_bucket_versioning]
+}
+
+# variables.tf
+variable "environment" {
+  description = "Environment (dev/staging/prod)"
+  type        = string
+  validation {
+    condition     = contains(["dev", "staging", "prod"], var.environment)
+    error_message = "Environment must be one of: dev, staging, prod"
+  }
+}
+
+variable "source_bucket" {
+  description = "Name suffix of the source data bucket (will be prefixed with nexabrands-{environment}-)"
+  type        = string
+}
+
+variable "target_bucket" {
+  description = "Name suffix of the target data bucket (will be prefixed with nexabrands-{environment}-)"
+  type        = string
+}
+
+variable "code_bucket" {
+  description = "Name suffix of the code bucket (will be prefixed with nexabrands-{environment}-)"
+  type        = string
+}
+
+variable "source_files" {
+  description = "Map of source files to upload to S3"
+  type        = map(string)
+  default = {
+    "customers.csv"         = "./modules/data/customers.csv"
+    "customer_targets.csv"  = "./modules/data/customer_targets.csv"
+    "dates.csv"            = "./modules/data/dates.csv"
+    "orders.csv"           = "./modules/data/orders.csv"
+    "order_fulfillment.csv" = "./modules/data/order_fulfillment.csv"
+    "order_lines.csv"      = "./modules/data/order_lines.csv"
+    "products.csv"         = "./modules/data/products.csv"
+  }
+}
+
+variable "code_files" {
+  description = "Map of code file names to their local paths for upload to code bucket under scripts/ directory"
+  type        = map(string)
+  default     = {}
+}
+
+variable "kms_deletion_window" {
+  description = "Duration in days before KMS key is deleted"
+  type        = number
+  default     = 7
+}
+
+variable "lifecycle_ia_transition_days" {
+  description = "Number of days before transitioning non-current versions to STANDARD_IA storage"
+  type        = number
+  default     = 30
+}
+
+variable "lifecycle_glacier_transition_days" {
+  description = "Number of days before transitioning non-current versions to GLACIER storage"
+  type        = number
+  default     = 60
+}
+
+variable "lifecycle_expiration_days" {
+  description = "Number of days before deleting non-current versions"
+  type        = number
+  default     = 90
+}
+
+variable "object_lock_retention_days" {
+  description = "Number of days for S3 Object Lock retention period in COMPLIANCE mode"
+  type        = number
+  default     = 1
 }
