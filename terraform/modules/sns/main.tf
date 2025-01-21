69ee2d09a083a34c314d119dafc7cbf1c5d3e765
Ajay263 <junioralexio607@gmail.com>
2025-01-21 18:38:07 +0200

    Clean up

--- terraform/modules/sns/main.tf
+++
@@ -1,6 +0,0 @@
-# modules/sns/main.tf
-resource "aws_sns_topic" "schema_changes" {
-  name = "topdevs-${var.environment}-schema-changes"
-}
-
-
--- terraform/modules/sns/outputs.tf
+++
@@ -1,7 +0,0 @@
-
-# modules/sns/outputs.tf
-output "topic_arn" {
-  value = aws_sns_topic.schema_changes.arn
-}
-
-
