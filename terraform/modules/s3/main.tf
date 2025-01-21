5e98237d6dba7a8a627e6719bfea7187b8dc1ce5
Ajay263 <junioralexio607@gmail.com>
2025-01-21 13:23:21 +0200

    Updated

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -232,38 +232,3 @@ variable "source_files" {
   }
 }
 
-variable "code_files" {
-  description = "Map of code file names to their local paths for upload to code bucket under scripts/ directory"
-  type        = map(string)
-  default     = {}
-}
-
-variable "kms_deletion_window" {
-  description = "Duration in days before KMS key is deleted"
-  type        = number
-  default     = 7
-}
-
-variable "lifecycle_ia_transition_days" {
-  description = "Number of days before transitioning non-current versions to STANDARD_IA storage"
-  type        = number
-  default     = 30
-}
-
-variable "lifecycle_glacier_transition_days" {
-  description = "Number of days before transitioning non-current versions to GLACIER storage"
-  type        = number
-  default     = 60
-}
-
-variable "lifecycle_expiration_days" {
-  description = "Number of days before deleting non-current versions"
-  type        = number
-  default     = 90
-}
-
-variable "object_lock_retention_days" {
-  description = "Number of days for S3 Object Lock retention period in COMPLIANCE mode"
-  type        = number
-  default     = 1
-}
