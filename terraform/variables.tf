5789c4646233228c45967b24f7bfffedd23c4fb3
Ajay263 <junioralexio607@gmail.com>
2025-01-23 21:36:53 +0200

    Removed the deleted scripts form the variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -206,8 +206,8 @@ variable "code_files" {
   description = "Map of code file names to their local paths"
   type        = map(string)
   default = {
-    "orders.py"         = "./modules/scripts/orders.py"
-  
+    "orders.py" = "./modules/scripts/orders.py"
+
   }
 }
 
