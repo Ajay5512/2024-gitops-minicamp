512189898641b20746d8ea33c7ac4bd25fa4be7c
Ajay263 <junioralexio607@gmail.com>
2025-01-23 12:12:45 +0200

    Updated the glue scripts

--- terraform/modules/glue/main.tf
+++ terraform/modules/glue/main.tf
@@ -31,68 +31,6 @@ resource "aws_glue_crawler" "crawler" {
   })
 }
 
-resource "aws_glue_job" "etl_job" {
-  name              = "topdevs-${var.environment}-etl-job"
-  role_arn          = var.glue_role_arn
-  glue_version      = "4.0"
-  worker_type       = "G.1X"
-  number_of_workers = 2
-  timeout           = 2880
-  max_retries       = 1
-
-  command {
-    name            = "glueetl"
-    python_version  = "3"
-    script_location = "s3://${var.code_bucket}/scripts/script.py"
-  }
-
-  default_arguments = {
-    "--enable-auto-scaling"              = "true"
-    "--enable-continuous-cloudwatch-log" = "true"
-    "--source-path"                      = "s3://${var.source_bucket}/"
-    "--destination-path"                 = "s3://${var.target_bucket}/"
-    "--job-name"                         = "topdevs-${var.environment}-etl-job"
-    "--enable-metrics"                   = "true"
-  }
-
-  execution_property {
-    max_concurrent_runs = 1
-  }
-
-  tags = {
-    Environment = var.environment
-    Service     = "glue"
-  }
-}
-
-
-# Variables
-variable "environment" {
-  description = "The environment (e.g., dev, staging, prod)"
-  type        = string
-}
-
-variable "glue_role_arn" {
-  description = "The ARN of the IAM role for Glue"
-  type        = string
-}
-
-variable "code_bucket" {
-  description = "The S3 bucket where the Glue scripts are stored"
-  type        = string
-}
-
-variable "source_bucket" {
-  description = "The S3 bucket for source data"
-  type        = string
-}
-
-variable "target_bucket" {
-  description = "The S3 bucket for processed data"
-  type        = string
-}
-
-# Glue Jobs
 locals {
   jobs = {
     products_etl        = "products_etl"
