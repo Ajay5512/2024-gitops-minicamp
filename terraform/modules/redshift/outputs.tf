dc292a66ea34ec86dbdd9bbf53735c97ccae0e4e
Ajay263 <junioralexio607@gmail.com>
2025-02-27 22:29:20 +0200

    Updated the outputs

--- terraform/modules/redshift/outputs.tf
+++ terraform/modules/redshift/outputs.tf
@@ -1,16 +1,14 @@
-# modules/redshift/outputs.tf
+output "redshift_endpoint" {
+  description = "The endpoint of the Redshift Serverless workgroup"
+  value       = aws_redshiftserverless_workgroup.serverless.endpoint
+}
 
-output "redshift_serverless_namespace_id" {
-  description = "The ID of the Redshift Serverless namespace."
+output "redshift_namespace_id" {
+  description = "The ID of the Redshift Serverless namespace"
   value       = aws_redshiftserverless_namespace.serverless.id
 }
 
-output "redshift_serverless_workgroup_id" {
-  description = "The ID of the Redshift Serverless workgroup."
+output "redshift_workgroup_id" {
+  description = "The ID of the Redshift Serverless workgroup"
   value       = aws_redshiftserverless_workgroup.serverless.id
-}
-
-output "redshift_serverless_endpoint" {
-  description = "The endpoint of the Redshift Serverless workgroup."
-  value       = aws_redshiftserverless_workgroup.serverless.endpoint[0].address
 }
--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -1,14 +1,29 @@
-output "redshift_endpoint" {
-  description = "The endpoint of the Redshift Serverless workgroup"
-  value       = aws_redshiftserverless_workgroup.serverless.endpoint
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = aws_vpc.redshift-serverless-vpc.id
 }
 
-output "redshift_namespace_id" {
-  description = "The ID of the Redshift Serverless namespace"
-  value       = aws_redshiftserverless_namespace.serverless.id
+output "security_group_id" {
+  description = "The ID of the security group"
+  value       = aws_security_group.redshift-serverless-security-group.id
 }
 
-output "redshift_workgroup_id" {
-  description = "The ID of the Redshift Serverless workgroup"
-  value       = aws_redshiftserverless_workgroup.serverless.id
+output "public_subnet_az1_id" {
+  description = "The ID of the public subnet in AZ1"
+  value       = aws_subnet.public_subnet_az1.id
+}
+
+output "public_subnet_id" {
+  description = "The ID of the public subnet (alias for public_subnet_az1_id)"
+  value       = aws_subnet.public_subnet_az1.id
+}
+
+output "subnet_ids" {
+  description = "List of all subnet IDs"
+  value = [
+    aws_subnet.redshift-serverless-subnet-az1.id,
+    aws_subnet.redshift-serverless-subnet-az2.id,
+    aws_subnet.redshift-serverless-subnet-az3.id,
+    aws_subnet.public_subnet_az1.id
+  ]
 }
