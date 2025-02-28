46eba75c0bb26026937ee07efb579f8103756872
Ajay263 <junioralexio607@gmail.com>
2025-02-28 08:51:03 +0200

    Added enviroment variables in iam

--- terraform/modules/iam/variables.tf
+++ terraform/modules/iam/variables.tf
@@ -14,11 +14,18 @@ variable "sns_topic_arn" {
   type = string
 }
 
+# modules/iam/variables.tf
+variable "environment" {
+  description = "Environment (dev/staging/prod)"
+  type        = string
+}
 
 variable "kms_key_arn" {
   description = "The ARN of the KMS key used for S3 encryption"
   type        = string
 }
+
+
 
 
 variable "aws_region" {
