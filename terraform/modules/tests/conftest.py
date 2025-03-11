028756a8b3aeceab9daed18845c445faac2c245a
Ajay263 <junioralexio607@gmail.com>
2025-03-11 20:27:17 +0200

    Added mock setup for AWS Glue modules

--- terraform/modules/tests/conftest.py
+++ terraform/modules/tests/conftest.py
@@ -5,3 +5,4 @@ from unittest.mock import MagicMock
 sys.modules["awsglue"] = MagicMock()
 sys.modules["awsglue.context"] = MagicMock()
 sys.modules["awsglue.job"] = MagicMock()
+sys.modules["awsglue.utils"] = MagicMock()
